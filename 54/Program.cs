1c184e68dbf994c6597e1061f4228693c54946e5
Alexandra <lebedeva1618@yandex.ru>
2022-12-24 23:52:41 +0300

    54, ранжирование в строках от макс к мин

---
+++ 54/Program.cs
@@ -0,0 +1,48 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 11); // [-10, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void RankingMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            int maxIndex = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, maxIndex])
+                    maxIndex = k;
+            }
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[i, maxIndex];
+            matrix[i, maxIndex] = temp;
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите размер двухмерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+Console.WriteLine("Начальный массив:");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("Конечный массив:");
+RankingMatrix(matrix);
+PrintMatrix(matrix);

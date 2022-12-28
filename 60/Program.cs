863473c64e1c47ee2b2d1de1e0dd1ab1ec2d80f6
Alexandra <lebedeva1618@yandex.ru>
2022-12-28 16:59:06 +0300

    60. Трехмерный массив

---
+++ 60/Program.cs
@@ -0,0 +1,41 @@
+﻿void InputMatrix(int[,,] matrix)
+{
+    int number = 10;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = number++;
+            }
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+                Console.Write($"{matrix[i, j, z]} ({i}, {j}, {z}) \t");
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
+
+
+Console.Clear();
+Console.WriteLine("Введите размер трехмерного массива: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] * size[1] * size[2] > 90)
+{
+    Console.WriteLine("Вы ошиблись.\nВведите заново размер трехмерного массива:");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,,] matrix = new int[size[0], size[1], size[2]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
